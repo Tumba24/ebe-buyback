82fde93b1cdedaa3a9f2221bd5a9919c1d0940d4
Bronsen Lee <bronsen55@gmail.com>
2022-05-02 18:36:47 -0600

    Configured to forward headers

--- server/src/App/Program.cs
+++ server/src/App/Program.cs
@@ -4,6 +4,7 @@ using Evebuyback.Data;
 using EveBuyback.App;
 using EveBuyback.Domain;
 using MediatR;
+using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,8 +17,15 @@ builder.Services.AddScoped<IRefinedContractItemAggregateRepository, InMemoryRefi
 builder.Services.AddScoped<IStationOrderSummaryAggregateRepository, InMemoryStationOrderSummaryAggregateRepository>();
 builder.Services.AddScoped<IStationRepository, InMemoryStationRepository>();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders =
+        ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+});
 
 var app = builder.Build();
+
+app.UseForwardedHeaders();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
